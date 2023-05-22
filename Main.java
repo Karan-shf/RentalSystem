5f2df2291cbb7442f9b85c90640b83bdd7aadb27
Karan-shf <118483211+Karan-shf@users.noreply.github.com>
2023-05-22 10:37:38 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,20 @@
 import java.util.ArrayList;
 import java.util.Date;
-//comment
-class Movie {
 
+abstract class Item {
     private int id;
     private String title;
     private String genre;
-    private String director;
-    private String cast;
     private Date releaseDate;
     private boolean isAvailable;
-    
-    public Movie(String title,String genre,String director,String cast,Date releaseDate,int id) {
+
+    public Item(String title,String genre,int id) {
         this.title = title;
         this.genre = genre;
-        this.director = director;
-        this.cast = cast;
-        this.releaseDate = releaseDate;
+        this.releaseDate = new Date();
         this.id = id;
+
+        isAvailable = true;
     }
 
     public int getId() {
@@ -29,12 +26,6 @@ class Movie {
     public String getGenre() {
         return genre;
     }
-    public String getDirector() {
-        return director;
-    }
-    public String getCast() {
-        return cast;
-    }
     public Date getReleaseDate() {
         return releaseDate;
     }
@@ -43,6 +34,79 @@ class Movie {
     }
     public void setAvailable(boolean isAvailable) {
         this.isAvailable = isAvailable;
+    }
+}
+
+class Movie extends Item {
+
+    private String director;
+    private String cast;
+    
+    public Movie(String title,String genre,int id,String director,String cast) {
+        
+        super(title,genre,id);
+
+        this.director = director;
+        this.cast = cast;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+    public String getCast() {
+        return cast;
+    }
+    
+    // public void rentMovie(Customer customer) {
+
+    //     MovieRental newRental = new MovieRental(this, customer, this.getId()+customer.getId());
+
+    //     customer.getRentals().add(newRental);
+
+    //     this.setAvailable(false);
+    // }
+}
+
+class Game extends Item {
+    
+    private String platform;
+    private String publisher;
+
+    public Game(String title,String genre,int id,String platform,String publisher) {
+        
+        super(title, genre, id);
+
+        this.platform = platform;
+        this.publisher = publisher;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+    public String getPublisher() {
+        return publisher;
+    }
+
+}
+
+class Book extends Item {
+
+    private String author;
+    private String publisher;
+    
+    public Book(String title,String genre,int id,String author,String publisher) {
+        
+        super(title, genre, id);
+
+        this.author = author;
+        this.publisher = publisher;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+    public String getPublisher() {
+        return publisher;
     }
 
 }
@@ -54,7 +118,7 @@ class Customer {
     private String email;
     private String phone;
     private String address;
-    ArrayList<Rental> rentals;
+    ArrayList<ItemRental> rentals;
 
     public Customer(String name,String email,String phone,String address,int id) {
         this.name = name;
@@ -79,34 +143,32 @@ class Customer {
     public String getAddress() {
         return address;
     }
-    public ArrayList<Rental> getRentals() {
+    public ArrayList<ItemRental> getRentals() {
         return rentals;
     }
 
 }
 
-class Rental {
+class ItemRental {
 
-    private Movie rentedMovie;
+    private Item item;
     private Customer customer;
     private Date rentalDate;
-    private Date returndDate;
+    private Date returnedDate;
     private int id;
 
-    public Rental(Movie rentedMovie,Customer customer,Date rentalDate,int id) {
-        this.rentedMovie = rentedMovie;
+    public ItemRental(Item item,Customer customer,int id) {
+        this.item = item;
         this.customer = customer;
-        this.rentalDate = rentalDate;
+        this.rentalDate = new Date();
         this.id = id;
-
-        rentedMovie.setAvailable(false);
     }
 
     public int getId() {
         return id;
     }
-    public Movie getRentedMovie() {
-        return rentedMovie;
+    public Item getItem() {
+        return item;
     }
     public Customer getCustomer() {
         return customer;
@@ -114,65 +176,140 @@ class Rental {
     public Date getRentalDate() {
         return rentalDate;
     }
-    public Date getReturndDate() {
-        return returndDate;
+    public Date getReturnedDate() {
+        return returnedDate;
     }
-    public void setReturndDate(Date returndDate) {
-        this.returndDate = returndDate;
+    public void setReturnedDate() {
+        this.returnedDate = new Date();
     }
+    
 
 }
 
+// class MovieRental extends ItemRental {
+
+//     private Movie rentedMovie;
+
+//     public MovieRental(Movie rentedMovie,Customer customer,int id) {
+//         super(customer, id);
+//         this.rentedMovie = rentedMovie;
+
+//         rentedMovie.setAvailable(false);
+//     }
+
+//     public Movie getRentedMovie() {
+//         return rentedMovie;
+//     }
+
+// }
+
+// class GameRental extends ItemRental {
+
+//     private Game rentedGame;
+
+//     public GameRental(Game rentedGame,Customer customer,int id) {
+//         super(customer, id);
+//         this.rentedGame = rentedGame;
+
+//         rentedGame.setAvailable(false);
+//     }
+
+//     public Game getRentedGame() {
+//         return rentedGame;
+//     }
+
+// }
+
+// class BookRental extends ItemRental {
+
+//     private Book rentedBook;
+
+//     public BookRental(Book rentedBook,Customer customer,int id) {
+//         super(customer, id);
+//         this.rentedBook = rentedBook;
+
+//         rentedBook.setAvailable(false);
+//     }
+
+//     public Book getRentedBook() {
+//         return rentedBook;
+//     }
+
+// }
+
 class RentalStore {
 
-    ArrayList<Movie> movies;
+    ArrayList<Item> Items;
     ArrayList<Customer> customers;
 
     public void register(Customer customer) {
         customers.add(customer);
     }
 
-    public void addMovie(Movie movie) {
-        movies.add(movie);
+    public void addItem(Item item) {
+        Items.add(item);
     }
 
-    public void removeMovie(Movie movie) {
-        movies.remove(movie);
+    public void removeItem(Item item) {
+        Items.remove(item);
     }
 
-    public ArrayList<Movie> getAvailableMovies() {
+    public ArrayList<Item> getAvailableItems() {
 
-        ArrayList<Movie> AvailableMovies = new ArrayList<>(movies);
+        ArrayList<Item> AvailableItems = new ArrayList<>(Items);
 
-        for (Movie movie : movies) {
-            if (movie.isAvailable()) {
-                AvailableMovies.add(movie);
+        for (Item item : Items) {
+            if (item.isAvailable()) {
+                AvailableItems.add(item);
             }
         }
 
-        return AvailableMovies;
+        return AvailableItems;
         
     }
 
-    public void rentMovie(Movie movie,Customer customer) {
-
-        Date date = new Date();
+    // public void rentMovie(Movie movie,Customer customer) {
         
-        Rental newRental = new Rental(movie, customer, date, movie.getId()+customer.getId());
+    //     // ItemRental newRental = new ItemRental(movie, customer, movie.getId()+customer.getId());
+    //     MovieRental newMovieRental = new MovieRental(movie, customer, movie.getId()+customer.getId());
 
-        customer.getRentals().add(newRental);
+    //     customer.getRentals().add(newMovieRental);
 
+    // }
+
+    // public void rentGame(Game game,Customer customer) {
+
+    //     GameRental newGameRental = new GameRental(game, customer, game.getId()+customer.getId());
+
+    //     customer.getRentals().add(newGameRental);
+        
+    // }
+
+    // public void rentBook(Book book,Customer customer) {
+        
+    //     BookRental newBookRental = new BookRental(book, customer, book.getId()+customer.getId());
+
+    //     customer.getRentals().add(newBookRental);
+
+    // }
+
+    public void rentItem(Item item, Customer customer) {
+
+        ItemRental newItemRental = new ItemRental(item,customer,item.getId()+customer.getId());
+
+        customer.getRentals().add(newItemRental);
+
+        item.setAvailable(false);
+            
     }
 
-    public void returnMovie(Rental rental) {
+    public void returnItem(ItemRental rental) {
 
-        Date date = new Date();
-
-        rental.setReturndDate(date);
+        rental.setReturnedDate();
 
         rental.getCustomer().getRentals().remove(rental);
 
-        rental.getRentedMovie().setAvailable(true);
+        rental.getItem().setAvailable(true);
         
     }
 
@@ -189,11 +326,11 @@ class RentalStore {
         
     }
 
-    public Movie getMovieById(int id) {
+    public Item getItemById(int id) {
 
-        for (Movie movie : movies) {
-            if (movie.getId()==id) {
-                return movie;
+        for (Item item : Items) {
+            if (item.getId()==id) {
+                return item;
             }
         }
 
@@ -204,9 +341,8 @@ class RentalStore {
 
 }
 
-
 public class Main {
     public static void main(String[] args) {
-        
+        //try me;
     }
 }
